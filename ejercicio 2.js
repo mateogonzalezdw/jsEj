c5c01d3f286af360171e1da25d790f35c373f1c6
mateogonzalezdw <mateogaelgonzalezbarila@gmail.com>
2025-03-17 17:00:45 -0300

    mi primer commit

---
+++ ejercicio 2.js
@@ -0,0 +1,60 @@
+
+function eje(){
+    let meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+for(let i=0;i<12;i++){
+console.log(meses[i])
+}
+}
+
+function eje2(){
+    var valores = [true, 5, false, "hola", "adios", 2];
+if(valores[3]>valores[4])
+{
+console.log(valores[4]+" es mayor a "+valores[3])
+console.log(valores[0])
+console.log(valores[1]+valores[5])
+console.log(valores[1]-valores[5])
+console.log(valores[1]*valores[5])
+console.log(valores[1]/valores[5])
+console.log(valores[1]**valores[5])
+}
+else if(valores[3]<valores[4])
+{
+    console.log(valores[3]+" es mayor a "+valores[4])
+    console.log(valores[2])    
+}
+}
+
+function eje3(){
+    var numero1 = 5;
+var numero2 = 8;
+if(numero1<numero2) {
+console.log(numero1+" no es mayor que "+ numero2);
+}
+if(numero2>0) {
+console.log(numero2+ " es positivo");
+}
+if(numero1>0) {
+console.log(numero1+ " no es negativo o distinto de cero");
+}
+if(numero1+1<numero2) {
+console.log("Incrementar en 1 unidad el valor de "+ numero1+" no lo hace mayor o igual que "+ numero2);
+}
+}
+
+function eje4(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+        'E', 'T'];
+}
+function eje5(){
+}
+function eje6(){
+}
+function eje7(){
+}
+function eje8(){
+}
+function eje9(){
+}
+function eje10(){
+}
